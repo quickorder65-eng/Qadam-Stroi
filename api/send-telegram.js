a9795e950445067829d86b2f5c32b27728ddddb5
quickorder65-eng <quickorder65@gmail.com>
2026-06-04 16:11:26 +0500

    Delete api/send-telegram.js

--- api/send-telegram.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(410).json({
-    success: false,
-    message: 'This endpoint has been removed. Use /api/save-lead instead.'
-  });
-}
